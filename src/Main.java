83e6260319bc138063fa51abacab48d68aeddf42
Wahid Khan <wahid.khan74@gmail.com>
2023-11-27 05:11:51 +0000

    src modified

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,16 @@
 public class Main {
     
-      @Test  
+    @Test  
     public void testCube(){  
         System.out.println("test case cube");  
         assertEquals(27,Calculation.cube(3));  
     }  
+    @Test  
+    public void testReverseWord(){  
+        System.out.println("test case reverse word");  
+        assertEquals("ym eman si nahk",Calculation.reverseWord("my name is khan");  
+    }  
+
     @Test  
     public void testReverseWord(){  
         System.out.println("test case reverse word");  
