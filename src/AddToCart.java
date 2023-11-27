81a3112f958bd5b0fb2c6d6f9a2f3116dca66b5b
Alzohar Tech <alzohartech@gmail.com>
2023-11-27 12:34:31 +0530

    Added Test file

---
+++ src/AddToCart.java
@@ -0,0 +1,15 @@
+public class AddToCart {
+    
+    @Test  
+    public void testCube(){  
+        System.out.println("test case cube");  
+        assertEquals(27,Calculation.cube(3));  
+    } 
+     
+    @Test  
+    public void testReverseWord(){  
+        System.out.println("test case reverse word");  
+        assertEquals("ym eman si nahk",Calculation.reverseWord("my name is khan");  
+    }  
+
+}
