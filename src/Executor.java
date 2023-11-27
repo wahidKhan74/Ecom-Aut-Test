34c4c2ee8cc32e3584b51f14944d199fdc6e7b6a
Wahid Khan <wahid.khan74@gmail.com>
2023-11-27 05:04:36 +0000

    added source and data file

--- src/Executor.java
+++ src/Executor.java
@@ -6,4 +6,11 @@ public class Executor {
         assertEquals(4,Calculation.findMax(new int[]{1,3,4,2}));  
         assertEquals(-2,Calculation.findMax(new int[]{-12,-3,-4,-2}));  
     }  
+
+    @Test  
+    public void testFindMax(){  
+        System.out.println("test case find max");  
+        assertEquals(4,Calculation.findMax(new int[]{1,3,4,2}));  
+        assertEquals(-2,Calculation.findMax(new int[]{-12,-3,-4,-2}));  
+    }  
 }
--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,13 @@
 public class Main {
     
+      @Test  
+    public void testCube(){  
+        System.out.println("test case cube");  
+        assertEquals(27,Calculation.cube(3));  
+    }  
+    @Test  
+    public void testReverseWord(){  
+        System.out.println("test case reverse word");  
+        assertEquals("ym eman si nahk",Calculation.reverseWord("my name is khan");  
+    }  
 }
